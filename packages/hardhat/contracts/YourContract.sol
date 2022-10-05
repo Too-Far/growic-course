d4249fc0b1da878bdfc63154a768e4db5cfd77a7
Ryan <rdturner40@gmail.com>
2022-10-04 23:23:40 -0400

    initial commit

---
+++ packages/hardhat/contracts/YourContract.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol"; 
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+
+contract YourContract {
+
+  event SetPurpose(address sender, string purpose);
+
+  string public purpose = "Building Unstoppable Apps!!!";
+
+  uint256 public x = 10005;
+
+  constructor() payable {
+    // what should we do on deploy?
+  }
+
+  function setPurpose(string memory newPurpose) public {
+      purpose = newPurpose;
+      console.log(msg.sender,"set purpose to",purpose);
+      emit SetPurpose(msg.sender, purpose);
+  }
+
+  // to support receiving ETH by default
+  receive() external payable {}
+  fallback() external payable {}
+}
