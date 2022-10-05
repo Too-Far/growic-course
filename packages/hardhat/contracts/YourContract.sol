2656e453a9cbf2adfb8b600b9a84e7b634ca4bd7
Ryan <rdturner40@gmail.com>
2022-10-05 10:22:37 -0400

    struct activity

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -3,6 +3,12 @@ pragma solidity >=0.8.0 <0.9.0;
 
 contract YourContract {
     mapping(address => uint256) public userToBalance;
+    mapping(address => KYCUserData) public addressToUserData;
+
+    struct KYCUserData {
+        string name;
+        uint256 age;
+    }
 
     constructor() {}
 
@@ -12,5 +18,13 @@ contract YourContract {
 
     function checkBalance(address user_) public view returns (uint256) {
         return userToBalance[user_];
+    }
+
+    function setUserDetails(string calldata name_, uint256 age_) public {
+        addressToUserData[msg.sender] = KYCUserData(name_, age_);
+    }
+
+    function getUserDetail() public view returns (KYCUserData memory) {
+        return addressToUserData[msg.sender];
     }
 }
